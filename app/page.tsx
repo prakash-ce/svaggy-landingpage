f2b931abc50a3ce884cb564fd1b6fac69e401968
prabirsingh <prabirsacademy@gmail.com>
2024-09-06 14:15:17 +0530

    Shadcn implimented

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-
-
 export default function Home() {
   return (
-    <h1>Hello world</h1>
-     );
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-center">Home Page (Testing Phase)</h1>
+    </main>
+  );
 }
